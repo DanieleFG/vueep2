6a0bc0144185e02c1980267982a86d460bea0bf8
Daniele Galvão <daniele-fg@hotmail.com>
2022-03-09 21:45:22 -0300

    primeiro commit

---
+++ projeto_manual/src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <b-col>
+
+     <b-card 
+    title="Curso de Vue.Js"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2">
+       <b-card-text>
+          Eu sei que nunca me deixou
+Eu sei que sempre esteve aqui comigo
+Livrando o meu coração do que não é Seu
+E eu sei que todas as noites tu vens
+Pra acalmar a minha mente
+E pondo no meu coração que eu sou Teu
+       </b-card-text>
+       <b-button variant='primary'>Comprar</b-button>
+     </b-card>
+      </b-col>
+    </div>
+
+</template>
+<script>
+    export default {
+
+    }
+</script>
---
+++ projeto_manual/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-container>
+    <b-row aliggn-v="center">
+      <card></card>
+      <card></card>
+      <card></card>
+      
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import Card from '@/components/Card.vue';
+
+export default {
+  name: 'Home',
+  components: {
+   Card
+  }
+}
+</script>
